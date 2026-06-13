cd8c95f5eba9d40e39a06ca5ed8435ee65629861
Tokelo Komane <komanetokelo@gmail.com>
2026-06-14 01:32:15 +0200

    Update 1. Silver_ddl_creating_tables.sql

--- Scripts /Silver/1. Silver_ddl_creating_tables.sql
+++ Scripts /Silver/1. Silver_ddl_creating_tables.sql
@@ -1,5 +1,7 @@
 USE Datawarehouse;
 -- Creating table for Datawarehouse Database 
+USE Datawarehouse;
+-- Creating table for Datawarehouse Database 
 
 IF OBJECT_ID ('silver.crm_cust_info','U') IS NOT NULL
 	DROP TABLE silver.crm_cust_info;
@@ -11,7 +13,7 @@ CREATE TABLE silver.crm_cust_info(
 	cst_marital_status NVARCHAR(50),
 	cst_gndr NVARCHAR(50),
 	cst_create_date DATE,
-	dwh_create_date DATETIMe2 DEFAULT GETDATE()
+	dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
 GO 
 IF OBJECT_ID ('silver.crm_prd_info','U') IS NOT NULL
@@ -19,12 +21,13 @@ IF OBJECT_ID ('silver.crm_prd_info','U') IS NOT NULL
 
 CREATE TABLE silver.crm_prd_info(
 prd_id INT,
+cat_id NVARCHAR(50),
 prd_key NVARCHAR(50),
-prd_nam NVARCHAR(50),
+prd_nm NVARCHAR(50),
 prd_cost INT,
 prd_line NVARCHAR(10),
-prd_start DATE,
-prd_end DATE,
+prd_start_dt DATETIME,
+prd_end_dt DATETIME,
 dwh_create_date DATETIMe2 DEFAULT GETDATE()
 );
 
@@ -36,9 +39,9 @@ CREATE TABLE silver.crm_sales_details(
 sls_ord_num NVARCHAR(20),
 sls_prd_key NVARCHAR(20),
 sls_cust_id INT,
-sls_order_dt INT,
-sls_ship_dt INT,
-sls_due_dt INT,
+sls_order_dt DATE,
+sls_ship_dt DATE,
+sls_due_dt DATE,
 sls_sales INT,
 sls_quantity INT,
 sls_price INT,
