e78f1c8ad989447951e1f2e4c5b0a7731477f377
Tokelo Komane <komanetokelo@gmail.com>
2026-07-01 15:14:07 +0200

    Update 1.  Creating_table.sql

--- Scripts /Bronze_layer/1.  Creating_table.sql
+++ Scripts /Bronze_layer/1.  Creating_table.sql
@@ -6,7 +6,7 @@ IF OBJECT_ID ('bronze.crm_cust_info','U') IS NOT NULL
 CREATE TABLE bronze.crm_cust_info(
 	cst_id INT,
 	cst_key NVARCHAR(50),
-	cst_first_name NVARCHAR(50), 
+	cst_firstname NVARCHAR(50), 
 	cst_lastname NVARCHAR(50),
 	cst_marital_status NVARCHAR(50),
 	cst_gndr NVARCHAR(50),
@@ -38,7 +38,7 @@ sls_order_dt INT,
 sls_ship_dt INT,
 sls_due_dt INT,
 sls_sales INT,
-sls_quantity INT,
+sls_qty INT,
 sls_price INT
 );
 
@@ -47,9 +47,9 @@ IF OBJECT_ID ('bronze.erp_cust_az12','U') IS NOT NULL
 	DROP TABLE bronze.erp_cust_az12;
 GO
 CREATE TABLE bronze.erp_cust_az12 (
-	cust_ID NVARCHAR(25),
-	bith_date DATE,
-	cust_gender NVARCHAR(10)
+	cst_ID NVARCHAR(25),
+	bdate DATE,
+	cst_gender NVARCHAR(10)
 
 );
 
@@ -58,17 +58,16 @@ IF OBJECT_ID ('bronze.erp_loc_a101','U') IS NOT NULL
 	DROP TABLE bronze.erp_loc_a101;
 GO
 CREATE TABLE bronze.erp_loc_a101(
-	cust_ID NVARCHAR(25),
-	country NVARCHAR(50)
+	cst_id NVARCHAR(25),
+	cntry NVARCHAR(50)
 );
 GO
 IF OBJECT_ID ('bronze.erp_px_cat_g1v2','U') IS NOT NULL
 	DROP TABLE bronze.erp_px_cat_g1v2;
 GO
 CREATE TABLE bronze.erp_px_cat_g1v2(
-	ID NVARCHAR(50),
-	category NVARCHAR(50),
-	sub_category NVARCHAR(50),
+	cst_id NVARCHAR(50),
+	cat NVARCHAR(50),
+	sub_cat NVARCHAR(50),
 	maintenance NVARCHAR(10)
 );
-
